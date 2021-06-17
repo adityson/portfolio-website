d8f6001af09dde417775938cd3adf951be26e2cb
Aditya Soni <adityasoni2000@gmail.com>
2021-06-17 20:04:55 +0530

    updated tools styles

--- components/Tools/ToolShowcase.tsx
+++ components/Tools/ToolShowcase.tsx
@@ -5,14 +5,14 @@ import { tools } from '../../data/tools';
 
 const ToolShowcase = () => {
     return (
-        <Flex flexDirection='column' marginBottom='24px'>
+        <Flex flexDirection='column' marginBottom='220px'>
             <Flex flexDirection='column' marginBottom={8}>
                 <Heading as='h3' size='2xl' marginTop='100px' textAlign='center' width='100%'>
                     Tools
                 </Heading>
                 <Text fontSize='sm' textAlign='center' marginTop='10px'>Tools that I find useful.</Text>
             </Flex>
-            <SimpleGrid columns={2}>
+            <SimpleGrid columns={2} mx={12} placeItems='center'>
                 {tools.map((tool) => (
                     <ToolSingle key={tool.id} toolInfo={tool} />
                 ))}
