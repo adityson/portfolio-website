8dbfd52ba22bacaa78415ae0d9d0cce3abb518a0
Aditya Soni <adityasoni2000@gmail.com>
2021-06-19 02:22:10 +0530

    post deployment changes

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -4,40 +4,35 @@ import { Flex, Heading, Text } from '@chakra-ui/react';
 import ProjectsShowcase from '../components/Projects/ProjectsShowcase';
 import { repoType } from './api/github';
 
-const projects = () => {
-    return(
-        <div></div>
+interface projectProps {
+    repos: repoType[];
+}
+
+const projects = ({ repos }: projectProps) => {
+    return (
+        <>
+        <Head>
+            <title>Projects|Portfolio</title>
+        </Head>
+        <Flex flexDirection='column'>
+            <Flex flexDirection='column'>
+                <Heading as='h2' size='4xl' marginTop='100px' textAlign='center' width='100%'>
+                    My Projects
+                </Heading>
+                <Text fontSize='sm' textAlign='center' marginTop='20px'>A brief showcase of the projects I made.</Text>
+            </Flex>
+            <ProjectsShowcase reposAll={repos} />
+        </Flex>
+        </>
     )
 }
 
-// interface projectProps {
-//     repos: repoType[];
-// }
-
-// const projects = ({ repos }: projectProps) => {
-//     return (
-//         <>
-//         <Head>
-//             <title>Projects|Portfolio</title>
-//         </Head>
-//         <Flex flexDirection='column'>
-//             <Flex flexDirection='column'>
-//                 <Heading as='h2' size='4xl' marginTop='100px' textAlign='center' width='100%'>
-//                     My Projects
-//                 </Heading>
-//                 <Text fontSize='sm' textAlign='center' marginTop='20px'>A brief showcase of the projects I made.</Text>
-//             </Flex>
-//             <ProjectsShowcase reposAll={repos} />
-//         </Flex>
-//         </>
-//     )
-// }
-
-// export async function getStaticProps() {
-//     const response = await fetch(`http://localhost:3000/api/github`);
-//     const { repos } = await response.json();
+export async function getStaticProps() {
+    const URL = process.env.NODE_ENV === 'production' ? `https://adityasoni.vercel.app` : `http://localhost:3000`;
+    const response = await fetch(`${URL}/api/github`);
+    const { repos } = await response.json();
     
-//     return { props: { repos, revalidate: 600 }};
-// }
+    return { props: { repos, revalidate: 600 }};
+}
 
 export default projects;
