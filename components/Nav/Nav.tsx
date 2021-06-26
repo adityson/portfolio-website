cc480e4beec38fe051ee214c3baeb167bf451c37
Aditya Soni <adityasoni2000@gmail.com>
2021-06-26 15:01:03 +0530

    updated nav styles and tech section

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,76 +1,117 @@
-import React, { useEffect } from 'react'
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-import { Flex, Heading, Box, Stack, useDisclosure } from '@chakra-ui/react';
-import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
-import LinkLi from './LinkLi';
-import LinkIcon from './LinkIcon';
-import { VscClose } from 'react-icons/vsc';
-import { FiMenu } from 'react-icons/fi';
-
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { Flex, Heading, Box, Stack, useDisclosure } from "@chakra-ui/react";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import LinkLi from "./LinkLi";
+import LinkIcon from "./LinkIcon";
+import { VscClose } from "react-icons/vsc";
+import { FiMenu } from "react-icons/fi";
 
 const Nav = () => {
+  const mobileNav = useDisclosure();
 
-    const mobileNav = useDisclosure();
+  const router = useRouter();
+  useEffect(() => {
+    mobileNav.onClose();
+  }, [router.pathname]);
 
-    const router = useRouter();
-    useEffect(() => {
-        mobileNav.onClose();
-    }, [router.pathname])
+  return (
+    <Flex
+      as="nav"
+      position="fixed"
+      alignItems="center"
+      wrap="wrap"
+      w="90%"
+      mx="5%"
+      borderRadius="20px"
+      justifyContent={{ base: "space-between", md: "space-around" }}
+      padding="8px 3%"
+      bgColor="#38B2AC"
+      color="white"
+    >
+      <Flex>
+        <Link href="/#intro">
+          <Heading
+            as="h3"
+            size="lg"
+            padding="8px"
+            textAlign="center"
+            width="200px"
+            margin={{ base: "0px", md: "0 20px" }}
+            borderRadius="15px"
+            transition="all 0.2s ease"
+            _hover={{ bgColor: "#2C7A7B", cursor: "pointer" }}
+          >
+            Aditya Soni
+          </Heading>
+        </Link>
+      </Flex>
+      <Box
+        display={{ base: "block", md: "none" }}
+        p="4px"
+        onClick={mobileNav.isOpen ? mobileNav.onClose : mobileNav.onOpen}
+        overflow="hidden"
+        borderRadius="6px"
+        border="1px solid #F0FFF4"
+        cursor="pointer"
+      >
+        {mobileNav.isOpen ? <VscClose /> : <FiMenu />}
+      </Box>
+      <Box
+        display={{ base: mobileNav.isOpen ? "block" : "none", md: "flex" }}
+        flexBasis={{ base: "100%", md: "auto" }}
+      >
+        {mobileNav.isOpen && (
+          <hr
+            style={{
+              background:
+                "linear-gradient(to right, #38B2AC, #C6F6D5, #38B2AC)",
+              height: "1px",
+              border: "0",
+              marginTop: "2px",
+            }}
+          />
+        )}
+        <Stack
+          spacing={4}
+          align="center"
+          justifyContent={[
+            "center",
+            "space-between",
+            "space-between",
+            "space-between",
+          ]}
+          direction={["column", "row", "row", "row"]}
+          pt={[2, 2, 0, 0]}
+          mx={[0, 0, 12, 12]}
+        >
+          {mobileNav.isOpen && <LinkLi to="/" LinkText="Home" />}
+          <LinkLi to="/projects" LinkText="Projects" />
+          <LinkLi to="/tech#tech" LinkText="Technologies" />
+        </Stack>
+        <Stack
+          spacing={4}
+          align="center"
+          justifyContent={[
+            "center",
+            "space-between",
+            "space-between",
+            "space-between",
+          ]}
+          direction={["column", "row", "row", "row"]}
+          pt={[4, 4, 0, 0]}
+        >
+          <LinkIcon to="https://github.com/adityson/">
+            <AiFillGithub size="36px" />
+          </LinkIcon>
+          <LinkIcon to="https://www.linkedin.com/in/soniaditya10/">
+            <AiFillLinkedin size="36px" />
+          </LinkIcon>
+        </Stack>
+      </Box>
+    </Flex>
+  );
+};
 
-    return (
-        <Flex as='nav' position='fixed' align='center' wrap='wrap' w='100%' justifyContent={{base: 'space-between', md: 'space-around'}} padding='10px 5%' bgColor='#38B2AC' color='white'>
-            <Flex>
-                <Link href='/#intro'>
-                    <Heading 
-                        as='h3'
-                        size='lg'
-                        padding='8px'
-                        textAlign='center'
-                        width='200px'
-                        margin={{base: '0px', md: '0 20px'}}
-                        borderRadius='15px'
-                        transition='all 0.2s ease'
-                        _hover={{bgColor: '#2C7A7B', cursor: 'pointer'}} 
-                    > 
-                        Aditya Soni
-                    </Heading>
-                </Link>
-            </Flex>
-            <Box display={{ base: 'block', md: 'none' }} p='4px' onClick={mobileNav.isOpen ? mobileNav.onClose : mobileNav.onOpen} overflow='hidden' borderRadius='6px' border='1px solid #F0FFF4' cursor='pointer'>
-                {mobileNav.isOpen ? <VscClose /> : <FiMenu />}
-            </Box>
-            <Box display={{ base: mobileNav.isOpen ? 'block' : 'none', md: 'flex'}} flexBasis={{ base: '100%', md: 'auto' }}>
-                {mobileNav.isOpen && <hr style={{ background: 'linear-gradient(to right, #38B2AC, #C6F6D5, #38B2AC)', height: '1px', border: '0', marginTop: '2px' }}/>}
-                <Stack 
-                    spacing={4} 
-                    align='center' 
-                    justifyContent={['center', 'space-between', 'space-between', 'space-between']} 
-                    direction={['column', 'row', 'row', 'row']}
-                    pt={[2,2,0,0]}
-                    mx={[0,0,12,12]}
-                >
-                    {mobileNav.isOpen && <LinkLi to='/' LinkText='Home' />}
-                    <LinkLi to='/projects' LinkText='Projects' />
-                    <LinkLi to='/tech#tech' LinkText='Technologies' />
-                </Stack>
-                <Stack 
-                    spacing={4} 
-                    align='center' 
-                    justifyContent={['center', 'space-between', 'space-between', 'space-between']} 
-                    direction={['column', 'row', 'row', 'row']}
-                    pt={[4,4,0,0]}
-                >
-                    <LinkIcon to='https://github.com/adityson/'>
-                        <AiFillGithub size='36px' />
-                    </LinkIcon>
-                    <LinkIcon to='https://linkedin.com/'>
-                        <AiFillLinkedin size='36px' />
-                    </LinkIcon>
-                </Stack>
-            </Box>
-        </Flex>
-    )
-}
-
-export default Nav
+export default Nav;
