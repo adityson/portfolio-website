60e47379bf94c6bc2dfe82920449d8edbbff6565
Aditya Soni <adityasoni2000@gmail.com>
2021-06-28 20:01:59 +0530

    updated image link and fixed missing prop in about

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,33 +1,46 @@
-import React from 'react'
-import { Flex, Heading, Text, Box } from '@chakra-ui/react';
-import BoxIn from './BoxIn';
+import React from "react";
+import { Flex, Heading, Text, Box } from "@chakra-ui/react";
+import BoxIn from "./BoxIn";
 
 const About = () => {
-    return (
-        <Flex id='about' flexDirection='column' alignItems='center' marginBottom='60px' mx='40px' paddingTop={{base: '100px', md: '0px'}}>
-            <Heading as='h2' size='3xl' marginBottom='36px' color='#065666'>About Me</Heading>
-            <Flex flexDirection='column' alignItems='center'>
-                <BoxIn childWidth='900px'>
-                    <Text fontSize={{base: 'md', md: 'xl'}}> 
-                        I'm a pre-final year undergrad student studying Computer Science and Engineering at NIT Silchar, India.
-                        I'm on the journey to explore the diverse field that Computer Science is. I love Web Development, trying out technologies,
-                        and building various features. This site is one of my projects I've put my heart into.
-                    </Text>
-                </BoxIn>
-                <BoxIn childWidth='750px'>
-                    <Text fontSize={{base: 'md', md: 'xl'}}> 
-                        Problem Solving using Data Structures and Algorithms fascinates me so I regularly keep up with that, 
-                        and I'm enthusiastic to dive deep to learn more.
-                    </Text>
-                </BoxIn>
-                <BoxIn childWidth='600px'>
-                    <Text fontSize={{base: 'md', md: 'xl'}}> 
-                        My interests include playing Tennis and Badminton, and learning Japanese (currently elementary level).
-                    </Text>
-                </BoxIn>
-            </Flex>
-        </Flex>
-    )
-}
+  return (
+    <Flex
+      id="about"
+      flexDirection="column"
+      alignItems="center"
+      marginBottom="60px"
+      mx="40px"
+      paddingTop={{ base: "100px", md: "0px" }}
+    >
+      <Heading as="h2" size="3xl" marginBottom="36px" color="#065666">
+        About Me
+      </Heading>
+      <Flex flexDirection="column" alignItems="center">
+        <BoxIn childWidth="900px" hasPartition={false}>
+          <Text fontSize={{ base: "md", md: "xl" }}>
+            I'm a pre-final year undergrad student studying Computer Science and
+            Engineering at NIT Silchar, India. I'm on the journey to explore the
+            diverse field that Computer Science is. I love Web Development,
+            trying out technologies, and building various features. This site is
+            one of my projects I've put my heart into.
+          </Text>
+        </BoxIn>
+        <BoxIn childWidth="750px" hasPartition={false}>
+          <Text fontSize={{ base: "md", md: "xl" }}>
+            Problem Solving using Data Structures and Algorithms fascinates me
+            so I regularly keep up with that, and I'm enthusiastic to dive deep
+            to learn more.
+          </Text>
+        </BoxIn>
+        <BoxIn childWidth="600px" hasPartition={false}>
+          <Text fontSize={{ base: "md", md: "xl" }}>
+            My interests include playing Tennis and Badminton, and learning
+            Japanese (currently elementary level).
+          </Text>
+        </BoxIn>
+      </Flex>
+    </Flex>
+  );
+};
 
 export default About;
--- data/projectRepos.ts
+++ data/projectRepos.ts
@@ -1,24 +1,24 @@
 export interface projectRepoType {
-    id: string;
-    name: string;
-    createMonth: string,
-    image?: string;
-    description: string;
+  id: string;
+  name: string;
+  createMonth: string;
+  image?: string;
+  description: string;
 }
 
 export const projectRepos: projectRepoType[] = [
-    {
-        id: 'portfolio-website',
-        name: 'Portfolio Website',
-        createMonth: `June '21 -`,
-        image: `https://raw.githubusercontent.com/adityson/portfolio-website/master/public/static/images/portfolioss.png`,
-        description: `You're here right now. I was learning NextJS so thought of making a website to showcase my own projects and achievements. This project is really close to me and I'll keep on learning as it grows.`
-    },
-    {
-        id: 'polls-fest',
-        name: 'Polls Fest',
-        createMonth: `May '21 - June '21`,
-        image: `https://raw.githubusercontent.com/adityson/polls-fest/master/pollsfest.png`,
-        description: `This was the first project I made with my own idea using ReactJS. Anyone can create, like, vote on any opinion poll. I thought of it as a way to know others' choices on anything that comes to mind.`
-    }
+  {
+    id: "portfolio-website",
+    name: "Portfolio Website",
+    createMonth: `June '21 -`,
+    image: `https://raw.githubusercontent.com/adityson/portfolio-website/master/public/static/images/portfolioss.png`,
+    description: `You're here right now. I was learning NextJS so thought of making a website to showcase my own projects and achievements. This project is really close to me and I'll keep on learning as it grows.`,
+  },
+  {
+    id: "polls-fest",
+    name: "Polls Fest",
+    createMonth: `May '21 - June '21`,
+    image: `https://raw.githubusercontent.com/adityson/polls-fest/master/pollsfest.png`,
+    description: `This was the first project I made with my own idea using ReactJS. Anyone can create, like, vote on any opinion poll. I thought of it as a way to know others' choices on anything that comes to mind.`,
+  },
 ];
